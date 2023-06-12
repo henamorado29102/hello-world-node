56d8e9bcbf17819e7450cdc4bf7a8ab9d876367a
Henrry Enamorado <henamorado29102@gmail.com>
2023-06-11 23:57:41 -0400

    add third endpoint

--- index.js
+++ index.js
@@ -9,4 +9,7 @@ app.get('/second', function (req, res) {
   res.send('add a second end point!!')
 })
 
+app.get('/third', function (req, res) {
+  res.send('add a third end point!!')
+})
 app.listen(3000)

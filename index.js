f839e27ac903978f434cb33daf15c754b474ae73
henrry.enamorado <henrry.enamorado@e-digital.cl>
2023-07-19 11:41:14 -0400

    update index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Henrry Enamorado Dieguez!')
+  res.send('Hello Henrry Enamorado Dieguez how are you!')
 })
 
 app.get('/second', function (req, res) {

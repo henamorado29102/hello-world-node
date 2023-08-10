4fd6d5862f1d2ad60b4787e5151811afeadb8788
henrry.enamorado <henrry.enamorado@e-digital.cl>
2023-08-10 14:54:10 -0400

    ooooo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send("Juena de arco en Cuba }}}}}}}   ");
+  res.send("Juena de arco en Cuba ////////   ");
 })
 
 app.get('/second', function (req, res) {
